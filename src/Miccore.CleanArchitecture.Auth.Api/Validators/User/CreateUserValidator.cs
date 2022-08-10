ed8aaeb627c5650769ef5360d56e586b217e809e
manher <61746673+MbakopManuel@users.noreply.github.com>
2022-08-10 18:07:24 +0100

    Update CreateUserValidator.cs

--- src/Miccore.CleanArchitecture.Auth.Api/Validators/User/CreateUserValidator.cs
+++ src/Miccore.CleanArchitecture.Auth.Api/Validators/User/CreateUserValidator.cs
@@ -19,11 +19,10 @@ namespace Miccore.CleanArchitecture.Auth.Api.Validators.User
                 .NotEmpty().WithMessage("PASSWORD_"+ValidatorEnum.NOT_EMPTY.ToString())
                 .NotNull().WithMessage("PASSWORD_"+ValidatorEnum.NOT_NULL.ToString())
                 .MinimumLength(8).WithMessage(ValidatorEnum.PASSWORD_MINIMUM_LENGTH_8.ToString())
-                .MaximumLength(16).WithMessage(ValidatorEnum.PASSWORD_MAX_LENGTH_16.ToString())
                 .Matches(@"[A-Z]+").WithMessage(ValidatorEnum.PASSWORD_MUST_CONTAIN_ONE_UPPERCASE_LETTER.ToString())
                 .Matches(@"[a-z]+").WithMessage(ValidatorEnum.PASSWORD_MUST_CONTAIN__ONE_LOWERCASE_LETTER.ToString())
                 .Matches(@"[0-9]+").WithMessage(ValidatorEnum.PASSWORD_MUST_CONTAIN_ONE_NUMBER.ToString())
-                .Matches(@"[\!\?\*\.]+").WithMessage(ValidatorEnum.PASSWORD_MUST_CONTAIN_AT_LEAST_SPECIAL_CHARS.ToString()+"_(!? *.)");
+                .Matches(@"[^A-Za-z0-9]+").WithMessage(ValidatorEnum.PASSWORD_MUST_CONTAIN_AT_LEAST_SPECIAL_CHARS.ToString());
             
             RuleFor(x => x.RoleId)
                 .NotEmpty()
